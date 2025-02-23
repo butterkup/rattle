00fe56be10219b40592995e4467c3bf601559510
Simon Nganga <sn.butterkup@gmail.com>
2025-02-23 19:05:17 +0300

    Implemented statement parsing for internal parser

---
+++ src/parser/parsers.cpp
@@ -0,0 +1,124 @@
+#include <cassert>
+#include <optional>
+#include <rattle/parser/parser.hpp>
+#include <rattle/token/token.hpp>
+#include <rattle/tree/nodes.hpp>
+#include <vector>
+
+namespace rattle::parser::internal {
+  Scoped<tree::Stmt> Parser::parse_stmt() noexcept {
+    for (;;) {
+      auto stmt = parse_statement();
+      if (stmt) {
+        return stmt;
+      }
+      switch (base.iskind()) {
+      case token::Kind::CloseBrace:
+        assert(not scopes.in_brace());
+        base.report(error::Kind::dangling_brace, base.eat());
+        break;
+      case token::Kind::CloseParen:
+        assert(not scopes.in_paren());
+        base.report(error::Kind::dangling_paren, base.eat());
+        break;
+      case token::Kind::CloseBracket:
+        assert(not scopes.in_bracket());
+        base.report(error::Kind::dangling_bracket, base.eat());
+        break;
+      case token::Kind::Eot:
+        return stmt;
+      default:
+        assert(false);
+      }
+    }
+  }
+
+  Scoped<tree::Block> Parser::parse_block() noexcept {
+    assert(base.iskind(token::Kind::OpenBrace));
+    auto lbrace = base.eat();
+    std::vector<Scoped<tree::Stmt>> stmts;
+    auto scope = scopes.enter_brace();
+    for (;;) {
+      auto stmt = parse_statement();
+      if (stmt) {
+        stmts.push_back(std::move(stmt));
+        continue;
+      }
+      switch (base.iskind()) {
+      case token::Kind::Eot:
+        // Malformed block statement, but okay for analysable.
+        base.report(error::Kind::unterminated_brace, lbrace);
+      case token::Kind::CloseBrace:
+        return make<tree::Block>(lbrace, base.eat(), std::move(stmts));
+      default:
+        assert(false); // Not reacheable!
+      }
+    }
+  }
+
+#define optional_parse_block()                                                 \
+  base.iskind(token::Kind::OpenBrace) ? parse_block() : nullptr
+
+  Scoped<tree::For> Parser::parse_for() noexcept {
+    assert(base.iskind(token::Kind::For));
+    auto kw = base.eat();
+    auto binding = parse_expression();
+    return make<tree::For>(kw, std::move(binding), optional_parse_block());
+  }
+
+  Scoped<tree::While> Parser::parse_while() noexcept {
+    assert(base.iskind(token::Kind::While));
+    auto kw = base.eat();
+    auto binding = parse_expression();
+    return make<tree::While>(kw, std::move(binding), optional_parse_block());
+  }
+
+  Scoped<tree::Def> Parser::parse_def() noexcept {
+    assert(base.iskind(token::Kind::Def));
+    auto kw = base.eat();
+    auto name_params = parse_expression();
+    return make<tree::Def>(kw, std::move(name_params), optional_parse_block());
+  }
+
+  Scoped<tree::Class> Parser::parse_class() noexcept {
+    assert(base.iskind(token::Kind::Class));
+    auto kw = base.eat();
+    auto name_bases = parse_expression();
+    return make<tree::Class>(kw, std::move(name_bases), optional_parse_block());
+  }
+
+  Scoped<tree::If> Parser::parse_if() noexcept {
+    assert(base.iskind(token::Kind::If));
+    auto kw = base.eat();
+    auto cond = parse_expression();
+    auto block = optional_parse_block();
+    tree::internal::If if_{kw, std::move(cond), std::move(block)};
+    std::optional<tree::internal::Else> else_ = std::nullopt;
+    if (base.iskind(token::Kind::Else)) {
+      auto kw2 = base.eat();
+      auto block2 = optional_parse_block();
+      else_.emplace(kw2, std::move(block));
+    }
+    return make<tree::If>(std::move(if_), std::move(else_));
+  }
+
+  Scoped<tree::Stmt> Parser::parse_assignment() noexcept {
+    auto left = parse_expression();
+    switch (base.iskind()) {
+#define rattle_pp_token_macro(kind, _) case token::Kind::kind:
+#include <rattle/token/require_pp/assignment.h>
+      break;
+    default:
+      if (left) {
+        parse_eos();
+        return make<tree::ExprStmt>(std::move(left));
+      } else {
+        return nullptr;
+      }
+    }
+    auto op = base.eat();
+    auto right = parse_expression();
+    return make<tree::Assignment>(op, std::move(left), std::move(right));
+  }
+} // namespace rattle::parser::internal
+
---
+++ src/parser/statement.cpp
@@ -0,0 +1,94 @@
+#include <rattle/parser/error.hpp>
+#include <rattle/parser/parser.hpp>
+#include <rattle/token/token.hpp>
+#include <rattle/tree/nodes.hpp>
+
+namespace rattle::parser::internal {
+  token::Token Parser::parse_eos() noexcept {
+    auto filter = base.with(State::EOS);
+    switch (base.iskind()) {
+    case token::Kind::Eot:
+    case token::Kind::Newline:
+    case token::Kind::Semicolon:
+      return base.eat(); // Own the EOS marker
+    default:
+      base.report(error::Kind::expected_eos_marker);
+      return base.peek(); // Might be anything, copy and let
+      // the parser figure out what it is.
+    }
+  }
+
+  Scoped<tree::Stmt> Parser::parse_statement() noexcept {
+    for (;;) {
+      switch (base.iskind()) {
+      case token::Kind::Continue: {
+        auto kw = base.eat();
+        parse_eos();
+        return make<tree::Continue>(kw);
+      }
+      case token::Kind::Break: {
+        auto kw = base.eat();
+        parse_eos();
+        return make<tree::Break>(kw);
+      }
+      case token::Kind::Return: {
+        auto kw = base.eat();
+        auto value = parse_expression();
+        parse_eos();
+        return make<tree::Return>(kw, std::move(value));
+      }
+      case token::Kind::Nonlocal: {
+        auto kw = base.eat();
+        auto names = parse_expression();
+        parse_eos();
+        return make<tree::Nonlocal>(kw, std::move(names));
+      }
+      case token::Kind::Global: {
+        auto kw = base.eat();
+        auto names = parse_expression();
+        parse_eos();
+        return make<tree::Global>(kw, std::move(names));
+      }
+      case token::Kind::OpenBrace:
+        return parse_block();
+      case token::Kind::Def:
+        return parse_def();
+      case token::Kind::Class:
+        return parse_class();
+      case token::Kind::If:
+        return parse_if();
+      case token::Kind::While:
+        return parse_while();
+      case token::Kind::For:
+        return parse_for();
+      case token::Kind::CloseBrace:
+        if (scopes.in_brace()) {
+          return nullptr; // Let caller consume closing token
+        } else {
+          base.report(error::Kind::dangling_brace, base.eat());
+          break;
+        }
+      case token::Kind::CloseParen:
+        if (scopes.in_paren()) {
+          return nullptr; // Let caller consume closing token
+        } else {
+          base.report(error::Kind::dangling_paren, base.eat());
+          break;
+        }
+      case token::Kind::CloseBracket:
+        if (scopes.in_bracket()) {
+          return nullptr; // Let caller consume closing token
+        } else {
+          base.report(error::Kind::dangling_bracket, base.eat());
+          break;
+        }
+      default:
+        if (auto stmt = parse_assignment()) {
+          return stmt;
+        }
+        base.report(error::Kind::unexpected_token, base.eat());
+      }
+    }
+  }
+} // namespace rattle::parser::internal
+
