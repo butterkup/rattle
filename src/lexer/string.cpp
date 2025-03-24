dbf18fd4fd33b9c3c2cf9ac173e0a180eac6efdb
Simon Nganga <sn.butterkup@gmail.com>
2025-03-24 20:19:02 +0300

    Lexer string lexer implementation

--- src/lexer/string.cpp
+++ src/lexer/string.cpp
@@ -1,18 +1,54 @@
-#include <cassert>
+#include "rattle/lexer/error.hpp"
+#include "rattle/rattle.hpp"
+#include "rattle/token/token.hpp"
+#include <optional>
 #include <rattle/lexer/lexer.hpp>
-#include <rattle/utility.hpp>
 
 namespace rattle::lexer::internal {
-  namespace strings {
-    // Handle in string escapes.
-    static void escape_sequence(Cursor &base, token::Kind &kind) noexcept {
-      auto mark = base.bookmark();
-      base.eat(); // Consume the escaper
-      if (base.empty()) {
-        kind = token::Kind::Error;
-        base.report(error::Kind::partial_string_escape, mark);
+  // Custom filter, lambda function getting too big. Hopefully compiler inlines
+  template <bool ismultiline, bool israw> struct Filter {
+    const char quote;
+    constexpr Filter(char quote) noexcept: quote(quote) {}
+    inline bool operator()(char ch) const noexcept { return invoke(ch); }
+    inline bool invoke(char ch) const noexcept;
+  };
+  // Overloads
+  template <> inline bool Filter<true, true>::invoke(char ch) const noexcept {
+    return ch != quote;
+  }
+  template <> inline bool Filter<true, false>::invoke(char ch) const noexcept {
+    return ch != quote and ch != '\\';
+  }
+  template <> inline bool Filter<false, true>::invoke(char ch) const noexcept {
+    return ch != quote and ch != '\n';
+  }
+  template <> inline bool Filter<false, false>::invoke(char ch) const noexcept {
+    return ch != quote and ch != '\n' and ch != '\\';
+  }
+
+  // String lexer
+  class String {
+    int flags;
+    Cursor &cursor;
+
+    // All errors are reported in one place to ensure error bit is activated
+    void report(error::Kind kind, std::optional<State> mark = std::nullopt) {
+      flags |= token::kinds::String::Error;
+      if (mark.has_value()) {
+        cursor.report(kind, mark.value());
       } else {
-        switch (base.peek()) {
+        cursor.report(kind);
+      }
+    }
+
+    // Handle in string escapes.
+    void escape_sequence() noexcept {
+      auto mark = cursor.bookmark();
+      cursor.eat(); // Consume the escaper
+      if (cursor.empty()) {
+        report(error::Kind::partial_string_escape, mark);
+      } else {
+        switch (cursor.peek()) {
         case '0':  // Null
         case 'n':  // Newline
         case 'r':  // Carriage return
@@ -24,112 +60,86 @@ namespace rattle::lexer::internal {
         case '\'': // Single quote
         case '"':  // Double quote
         case '\\': // Backward slash
-          base.eat();
+          cursor.eat();
           break;
         case 'x':
         case 'X': // Hexadecimal escape: \xab \x23 \x10
-          if (base.safe(2)) {
-            if (not(utility::is_hexadecimal(base.peek(1)) and
-                    utility::is_hexadecimal(base.peek(2)))) {
-              kind = token::Kind::Error;
-              base.report(error::Kind::invalid_escape_hex_sequence, mark);
+          if (cursor.safe(2)) {
+            if (not(utility::is_hexadecimal(cursor.peek(1)) and
+                    utility::is_hexadecimal(cursor.peek(2)))) {
+              report(error::Kind::invalid_escape_hex_sequence, mark);
             } else {
-              base.eat();
-              base.eat();
+              cursor.eat();
+              cursor.eat();
             }
           } else {
-            kind = token::Kind::Error;
-            base.report(error::Kind::partial_string_hex_escape, mark);
+            report(error::Kind::partial_string_hex_escape, mark);
           }
-          base.eat();
+          cursor.eat();
           break;
         default:
-          base.eat();
-          base.report(error::Kind::invalid_escape_sequence, mark);
+          cursor.eat();
+          report(error::Kind::invalid_escape_sequence, mark);
         }
       }
     }
-    // Custom filter, lambda function getting too big. Hopefully compiler inlines
-    template <bool ismultiline, bool israw> struct Filter {
-      const char quote;
-      constexpr Filter(char quote) noexcept: quote(quote) {}
-      bool operator()(char ch) const noexcept { return invoke(ch); }
-      bool invoke(char ch) const noexcept;
-    };
-    // Overloads
-    template <> inline bool Filter<true, true>::invoke(char ch) const noexcept {
-      return ch != quote;
-    }
-    template <>
-    inline bool Filter<true, false>::invoke(char ch) const noexcept {
-      return ch != quote and ch != '\\';
-    }
-    template <>
-    inline bool Filter<false, true>::invoke(char ch) const noexcept {
-      return ch != quote and ch != '\n';
-    }
-    template <>
-    inline bool Filter<false, false>::invoke(char ch) const noexcept {
-      return ch != quote and ch != '\n' and ch != '\\';
-    }
+
     // Generic function to lex strings. Can handle multiline and single
     // line string lexing and the compiler can evaluate some portions
     // (expressions with multiline constant) of the function removing
     // dead code and optimize for each listed variant.
     template <bool ismultiline, bool israw>
-    static token::Kind consume_string_variant(Cursor &base) noexcept {
-      const char quote = base.eat();
-      token::Kind kind = israw ? token::Kind::RawSingleLineString :
-                                 token::Kind::SingleLineString;
-      if constexpr (ismultiline) {
-        kind = israw ? token::Kind::RawMultilineString :
-                       token::Kind::MultilineString;
-        base.eat();
-        base.eat();
+    void consume_string_variant() noexcept {
+      const char quote = cursor.eat();
+      if constexpr (israw) {
+        flags |= token::kinds::String::Raw;
       }
+      if constexpr (ismultiline) {
+        flags |= token::kinds::String::Multiline;
+        cursor.eat();
+        cursor.eat();
+      }
+
       for (;;) {
-        base.eat_while(Filter<ismultiline, israw>(quote));
-        if (base.empty()) {
-          kind = token::Kind::Error;
-          base.report(ismultiline ?
-                        error::Kind::unterminated_multi_line_string :
-                        error::Kind::unterminated_single_line_string);
-          return kind;
+        cursor.eat_while(Filter<ismultiline, israw>(quote));
+        if (cursor.empty()) {
+          report(ismultiline ? error::Kind::unterminated_multi_line_string :
+                               error::Kind::unterminated_single_line_string);
+          return;
         } else {
-          switch (base.peek()) {
+          switch (cursor.peek()) {
           case '\\':
             if constexpr (israw) {
               // raw strings escape anything
-              auto mark = base.bookmark();
-              base.eat(); // Consume escaper
-              if (base.safe()) {
-                base.eat(); // Whatever the escaped is, consume it!
+              auto mark = cursor.bookmark();
+              cursor.eat(); // Consume escaper
+              if (cursor.safe()) {
+                cursor.eat(); // Whatever the escaped is, consume it!
               } else {
-                base.report(error::Kind::partial_string_escape, mark);
+                report(error::Kind::partial_string_escape, mark);
               }
             } else {
-              escape_sequence(base, kind);
+              escape_sequence();
             }
             break;
           case '\n':
             if constexpr (ismultiline) {
               // multiline strings ignore newlines
-              assert(false);
+              unreachable();
             } else {
-              kind = token::Kind::Error;
-              base.report(error::Kind::unterminated_single_line_string);
-              return kind;
+              report(error::Kind::unterminated_single_line_string);
+              return;
             }
             break;
           default:
             if constexpr (ismultiline) {
-              if (base.match(quote) and base.match(quote) and
-                  base.match(quote)) {
-                return kind;
+              if (cursor.match(quote) and cursor.match(quote) and
+                  cursor.match(quote)) {
+                return;
               }
             } else {
-              if (base.match(quote)) {
-                return kind;
+              if (cursor.match(quote)) {
+                return;
               }
             }
           }
@@ -139,22 +149,37 @@ namespace rattle::lexer::internal {
 
     // Determine which string variant to call and delegate lexing to it
     // forming a token on the way out
-    template <bool raw>
-    static token::Token consume_string(Cursor &base) noexcept {
-      const char quote = base.peek();
-      return base.make_token(
-        base.safe(2) and base.peek(1) == quote and base.peek(2) == quote ?
-          consume_string_variant<true, raw>(base) :
-          consume_string_variant<false, raw>(base));
+    template <bool israw> token::Token lex_string() noexcept {
+      flags = token::kinds::String::Normal;
+      const char quote = cursor.peek();
+      if (cursor.safe(2) and cursor.peek(1) == quote and
+          cursor.peek(2) == quote) {
+        consume_string_variant<true, israw>();
+      } else {
+        consume_string_variant<false, israw>();
+      }
+      return cursor.make_token(token::kinds::Token::String, flags);
     }
-  } // namespace strings
 
-  // A Facade; hides all the string lexing complexity from Cursor
-  token::Token Lexer::consume_string() noexcept {
-    return strings::consume_string<false>(base);
-  }
-  token::Token Lexer::consume_raw_string() noexcept {
-    return strings::consume_string<true>(base);
+  public:
+    String(Cursor &cursor): flags{0}, cursor{cursor} {}
+    // Cannot accept flags from outside since it would invalidate the lexer, but
+    // some flags are pretty necessary for checking for certain events like if
+    // syntactically the string should be lexed as a raw string or multiline as seen
+    // here and in the lex_string functions
+    token::Token lex(int flags) {
+      if (flags & token::kinds::String::Raw) {
+        return lex_string<true>();
+      } else {
+        return lex_string<false>();
+      }
+    }
+  };
+
+  // A facade, hides all string lexing weirdness from the lexer
+  token::Token Lexer::consume_string(int flags) noexcept {
+    // Call the actual lexer giving it lexer provided flags
+    return String(cursor).lex(flags);
   }
 } // namespace rattle::lexer::internal
 
